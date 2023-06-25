307319df7b2f595069678106f7e7c138f25c8eac
splemoine8 <splemoine8@gmail.com>
2023-06-25 14:33:14 -0600

    Revert "upload"
    This reverts commit edfc943f583ecfc7dcfc2d2865773da8712c2ad6.

--- components/Footer.js
+++ components/Footer.js
@@ -1,29 +1,11 @@
-// components/footer.js
-import Image from 'next/image';
-import styles from './footer.module.css';
+import styles from './Footer.module.css'
 
-const Footer = () => {
+export default function Footer() {
   return (
-    <div className={styles.footer}>
-      <div className={styles.footerContainer}>
-        <img
-          src="/images/leavesleft.png"
-          alt="Image Left"
-          className={styles.footerImageLeft}
-        />
-        <img
-          src="/images/leavesright.png"
-          alt="Image Right"
-          className={styles.footerImageRight}
-        />
-      </div>
-      <p className={styles.footerText} class="py-5">
-        Crafted by Scott with lots of &nbsp;
-        <span className={`fa fa-heart fa-beat ${styles.redHeart}`}></span>
-        &nbsp; for Maddy
-      </p>
-    </div>
-  );
-};
-
-export default Footer;
+    <>
+      <footer className={styles.footer}>
+        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+      </footer>
+    </>
+  )
+}
