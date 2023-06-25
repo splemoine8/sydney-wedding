edfc943f583ecfc7dcfc2d2865773da8712c2ad6
splemoine8 <splemoine8@gmail.com>
2023-06-25 14:28:30 -0600

    upload
    upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import '@styles/globals.css'
+import '../styles/globals.css';
+import '../styles/mapStyles.css';
+import '@fortawesome/fontawesome-free/css/all.min.css'; 
 
-function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default Application
+export default function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />
+  }
